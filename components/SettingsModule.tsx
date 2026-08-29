8021aa84b44febb775d8710311594e946443fe0b
marcopassarela <marcopassarela@hotmail.com>
2026-08-29 19:48:05 -0300

    update font

--- components/SettingsModule.tsx
+++ components/SettingsModule.tsx
@@ -408,7 +408,7 @@ export default function SettingsModule({ user }: { user: any }) {
                   <input
                     value={auditAction}
                     onChange={(e) => setAuditAction(e.target.value)}
-                    className="w-full rounded-lg border p-2 placeholder:text-[-11px]"
+                    className="w-full rounded-lg border p-2 placeholder:text-sm"
                     placeholder="LOGIN, CADASTRO…"
                   />
                 </label>
