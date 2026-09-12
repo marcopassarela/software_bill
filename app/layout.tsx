c66b4cd568637ea09947d20fefe06a33083704a7
marcopassarela <marcopassarela@hotmail.com>
2026-09-11 21:34:02 -0300

    update icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,10 @@ export const metadata: Metadata = {
     title: 'Logísticas Bill',
   },
   icons: {
-    icon: [{ url: '/icon2.png', type: 'image/png' }],
+    icon: [
+      { url: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/icon-512.png', sizes: '512x512', type: 'image/png' },
+    ],
     apple: [
       { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
     ],
